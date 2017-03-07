921d01e3c8ded1158595a722ed2b157cbf91c612
YaleX <alexecorzo@hotmail.com>
2017-03-07 19:59:40 +0000

    views and  sessions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @page_tittle = "Conciencia Colectiva"
+  	@page_description = "Plataforma de encuentro, diálogo y empoderamiento de las personas que comparten en pensamiento y en corazón, el vivir en un país mejor."
   end
   def contact
   end
@@ -11,12 +13,5 @@ class PagesController < ApplicationController
   end
   def hablemos
   end
-  def seamos2
-  end
-  def sonemos2
-  end
-  def actuamos2
-  end
-  def hablemos2
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'pages#home'
+  
   get 'contact' => 'pages#contact'
   get 'seamos' => 'pages#seamos', as: :seamos
+  get 'sonemos' => 'pages#sonemos', as: :sonemos
+  get 'actuamos' => 'pages#actuamos', as: :actuamos
+  get 'hablemos' => 'pages#hablemos', as: :hablemos
+
   get 'seamos2' => 'pages#seamos2', as: :seamos2
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
