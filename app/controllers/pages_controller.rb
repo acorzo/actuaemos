9dac550c6184cfd202bdd446d4bc25b22365c87f
YaleX <alexecorzo@hotmail.com>
2017-03-22 15:17:38 +0000

    template leads default

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @page_tittle = "Conciencia Colectiva"
   	@page_description = "Plataforma de encuentro, diálogo y empoderamiento de las personas que comparten en pensamiento y en corazón, el vivir en un país mejor."
+    @lead =Lead.new
   end
   def contact
   end
